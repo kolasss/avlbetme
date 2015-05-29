20417effda197d2295d8b1281694b2d028bfe252
kolasss <kolas.batman@gmail.com>
2015-05-29 16:46:37 +0900

    unit tests for user, stake_type

--- test/models/stake_type_test.rb
+++ test/models/stake_type_test.rb
@@ -12,7 +12,29 @@
 require 'test_helper'
 
 class StakeTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @stake_type = StakeType.new(
+      title: "Доллар",
+      numeric: true
+    )
+  end
+
+  test "should be valid" do
+    assert @stake_type.valid?
+  end
+
+  test "title should be present" do
+    @stake_type.title = "     "
+    assert_not @stake_type.valid?
+  end
+
+  test "should not allow destroy with exist stake" do
+    @stake_type.save
+    @stake_type.stakes.create(
+      user_id: 1,
+      bet_id: 1,
+      bid: 'test'
+    )
+    assert_not @stake_type.destroy
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,7 +20,60 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      name: "Николай Аммосов",
+      photo: "http://cs623631.vk.me/v623631476/1cbe5/xovD7YVxZKU.jpg",
+      friends: {"vk_friends_ids"=>[54304, 95663]}
+    )
+    @vk_authentication = @user.authentications.build(
+      provider: 'vk',
+      uid: '3750476'
+    )
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "should have authentication" do
+    assert @user.authentications.any?
+  end
+
+  test "vk_id should match authentication uid" do
+    @user.save
+    assert_equal @user.vk_id, @vk_authentication.uid
+  end
+
+  test "associated authentications should be destroyed" do
+    @user.save
+    assert_difference 'Authentication.count', -1 do
+      @user.destroy
+    end
+  end
+
+  test "friends_list should return right users" do
+    depp = users(:depp)
+    pit = users(:pit)
+    jolie = users(:jolie)
+
+    assert depp.friends_list.include?(pit)
+    assert_not depp.friends_list.include?(jolie)
+  end
+
+  test "should not allow destroy with exist stake" do
+    @user.save
+    @user.stakes.create(
+      stake_type_id: 1,
+      bet_id: 1,
+      bid: 'test'
+    )
+    assert_not @user.destroy
+  end
+
 end
